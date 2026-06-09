827b165829f467d1761957e945a34882b93eb05a
prasanna <prasanna0324@gmail.com>
2026-06-09 15:04:20 -0500

    connected to live render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://turfplay-backend.onrender.com/api",
+  baseURL: "https://turfplay-backend-2.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
